b5b73fc76e53297d9bdf9ad27c0d04f22090a3a0
Jason Ostergren <jason.ostergren79@gmail.com>
2019-08-15 21:59:51 -0500

    continued work on js, have board updating now, working on storing in local forage and data tweaks on bugged fields

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,59 @@
+document.querySelector("button").addEventListener("click" ,function() {
+    console.log("youclicked");
+    submitInfo()
+
+});
 
 
-document.querySelector("button").addEventListener("click", function(event) {
-    event.preventDefault();
-    
-    function submitInfo() {
-    
-    let trainName = document.getElementById("train-name").value;
-    let destination = document.getElementById("train-destination").value;
-    let firstTime = document.getElementById("first-time").value;
-    let frequencyMins = document.getElementById("frequency").value;
-    let firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    let currentTime = moment();
-    let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    let tRemainder = diffTime % frequency;
-    let tMinutesTillTrain = frequency - tRemainder;
-    let nextTrain = moment().add(tMinutesTillTrain, "minutes");
+function submitInfo() {
+
+    var trainName = document.getElementById("train-name").value;
+    var destination = document.getElementById("train-destination").value;
+    var firstTime = document.getElementById("first-time").value;
+    var frequencyMins = document.getElementById("trainFrequency").value;
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var timeAway = diffTime % frequency;
+    var minutesAway = frequency - timeAway;
+    var nextTrain = moment().add(minutesAway, "minutes");
         
     console.log(trainName);
         console.log(destination);
         console.log(frequency);
         console.log(moment(nextTrain).format("hh:mm"));
-        console.log(tMinutesTillTrain);
+        console.log(minutesAway);
 
-    let tableRef = document.getElementById("trainTable").getElementsByTagName('tbody')[0];
+    var tableRef = document.getElementById("trainTable").getElementsByTagName("tbody")[0];
 
-    let newRow   = tableRef.insertRow();
+    var newRow   = tableRef.insertRow();
 
-    let newCell1  = newRow.insertCell(0);
-    let newCell2  = newRow.insertCell(1);
-    let newCell3  = newRow.insertCell(2);
-    let newCell4  = newRow.insertCell(3);
-    let newCell5  = newRow.insertCell(4);
+    var newCell1  = newRow.insertCell(0);
+    var newCell2  = newRow.insertCell(1);
+    var newCell3  = newRow.insertCell(2);
+    var newCell4  = newRow.insertCell(3);
+    var newCell5  = newRow.insertCell(4);
 
-    let newText1  = document.createTextNode(trainName);
+    var newText1  = document.createTextNode(trainName);
     newCell1.appendChild(newText1);
-    let newText2 = document.createTextNode(destination);
+    var newText2 = document.createTextNode(destination);
     newCell2.appendChild(newText2);
-    let newText3  = document.createTextNode(frequency);
+    var newText3  = document.createTextNode(frequency);
     newCell3.appendChild(newText3);
-    let newText4  = document.createTextNode(moment(nextTrain).format("hh:mm"));
+    var newText4  = document.createTextNode(moment(nextTrain).format("hh:mm"));
     newCell4.appendChild(newText4);
-    let newText5  = document.createTextNode(tMinutesTillTrain);
+    var newText5  = document.createTextNode(minutesAway);
     newCell5.appendChild(newText5);
-    }
-    submitInfo()
-});
+}
+    
+   
+
+
+
+
+
+   
+
 
     
 
